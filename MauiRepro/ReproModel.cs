bff0927589cf4aa30973ffd6200341be1f3d7bc9
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2022-03-12 01:35:19 +0200

    Add files via upload

---
+++ MauiRepro/ReproModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace MauiRepro
+{
+    public class ReproModel
+    {
+        public ItemModel[] Items => Enumerable.Repeat(new[] { 1, 2, 3 }, 1000)
+            .SelectMany(i => i)
+            .Select(i => new ItemModel(i))
+            .ToArray();
+    }
+
+    public class ItemModel
+    {
+        int i;
+
+        public ItemModel(int i)
+        {
+            this.i = i;
+        }
+
+        public bool OneEnabled => i == 1;
+        public bool TwoEnabled => i == 2;
+        public bool ThreeEnabled => i == 3;
+    }
+}
